f16e79e41871a0a0facc0bd1598784527efcf9a9
Noel Anstey <anstey.ng@gmail.com>
2017-03-11 22:12:50 +0000

    implement simple calculator

--- poppinBottles.js
+++ poppinBottles.js
@@ -10,3 +10,14 @@ function calcBottleCapReturn(caps){
   return Math.floor( caps / 4 );
 }
 
+function returnOnInvestment(dollars){
+  var bottles = calcBottlesPurchase(dollars);
+  var emptyReturn = calcEmptyBottleReturn(bottles);
+  var capReturn = calcBottleCapReturn(bottles);
+  console.log('With $' + dollars + ' you can buy ' + bottles + ' bottles.');
+  console.log(bottles + ' empty bottles will get you ' + emptyReturn + ' free bottles.');
+  console.log(bottles + ' bottle caps will get you ' + capReturn + ' free bottles.');
+}
+
+var dollars = process.argv[2];
+returnOnInvestment( dollars );
