81adeee9c9e6c933e5e2e3eabbe71967c728fa64
Noel Anstey <anstey.ng@gmail.com>
2017-03-11 21:57:33 +0000

    base functions

---
+++ poppinBottles.js
@@ -0,0 +1,12 @@
+function calcBottlesPurchase(dollars){
+  return Math.floor( dollars / 2 );
+}
+
+function calcEmptyBottleReturn(empties){
+  return Math.floor( empties / 2 );
+}
+
+function calcBottleCapReturn(caps){
+  return Math.floor( caps / 4 );
+}
+
